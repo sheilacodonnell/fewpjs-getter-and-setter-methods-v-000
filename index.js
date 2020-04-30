ade4dd5551098ae6cb1870c21953f6b92c93b22f
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-30 15:57:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,4 +14,12 @@ class Circle {
   get area() {
     return Math.PI * (this.radius * this.radius);
   }
+  
+  set diameter(diameter) {
+    this.radius = diameter / 2
+  }
+
+  set circumference(circumference) {
+    this.radius = circumference / (2 * Math.PI)
+  }
 }
